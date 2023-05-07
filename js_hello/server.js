7ebafbd3e424ef440f2dbcae4e7ad8d5e82fd4f8
dejanualex <dejanualexandru@gmail.com>
2023-05-07 23:32:00 +0300

    add js app

--- js_hello/server.js
+++ js_hello/server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello World!");
+  response.end("Hello World JS!");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
