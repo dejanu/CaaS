073d56a7cad6fab21a900783a9cddc8a786711ec
dejanualex <dejanualexandru@gmail.com>
2023-05-07 23:24:01 +0300

    refactor go app

--- go_hello/main.go
+++ go_hello/main.go
@@ -2,21 +2,18 @@ package main
 
 import (
     "fmt"
-    "html"
-    "log"
     "net/http"
 )
 
 func main() {
 
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+        fmt.Fprintf(w, "Hello world!")
     })
 
     http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
         fmt.Fprintf(w, "Hi")
     })
 
-    log.Fatal(http.ListenAndServe(":8081", nil))
-
+    http.ListenAndServe(":8080", nil)
 }
