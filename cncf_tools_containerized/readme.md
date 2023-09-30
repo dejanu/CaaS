a28a22607cc73d25038f61657565e3cedf9844cd
dejanualex <dejanualexandru@gmail.com>
2023-09-30 23:25:46 +0300

    some alignment

--- cncf_tools_containerized/readme.md
+++
@@ -1,6 +0,0 @@
-## Folder containing Dockerfiles for different tools and purporses
-
-### skopeo
-
-* [Documentation](https://github.com/containers/skopeo/blob/main/install.md)
-
--- js_hello/server.js
+++ js_hello/server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello World JS!");
+  response.end("<p><b>Hello World JS!</b></p>");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
