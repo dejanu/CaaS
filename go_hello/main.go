734e053cb904c1342f3bc82538a8019464d12dfb
dejanualex <dejanualexandru@gmail.com>
2023-12-28 15:52:34 +0200

    dockerfile play

--- go_hello/main.go
+++ go_hello/main.go
@@ -10,11 +10,10 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// fmt.Fprintf(w, "<p><hr><center>GO <b>CAT</b>🐱</center></p>")
-		fmt.Fprintf(w, "<p><hr><center>GO <b>LION</b>🦁</center></p>")
+		// fmt.Fprintf(w, "<p><hr><center>GO <b>LION</b>🦁</center></p>")
+		// serve static file
+		http.ServeFile(w, r, "./static/gopher_dance.gif")
 		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
-
-		// // server static file
-		// http.ServeFile(w, r, "./static/gopher_dance.gif")
 	})
 
 	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
