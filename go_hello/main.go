b5dbb5a25748cb13bfc38da574302a157be6f102
dejanu <dejanualexandru@gmail.com>
2025-01-20 17:48:47 +0200

    update script

--- go_hello/main.go
+++ go_hello/main.go
@@ -7,21 +7,19 @@ import (
 )
 
 func main() {
-
+	// / endpoint
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// fmt.Fprintf(w, "<p><hr><center>GO <b>CAT</b>🐱</center></p>")
 		// fmt.Fprintf(w, "<p><hr><center>GO <b>LION</b>🦁</center></p>")
-		// serve static file
-		http.ServeFile(w, r, "./static/gopher_dance.gif")
+		http.ServeFile(w, r, "./static/gopher_dance.gif") // serve static file
 		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
 	})
 
+	// /hi endpoint
 	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hellooo woooorld")
 	})
 
-	log.Fatal(
-		http.ListenAndServe(":8888", nil),
-	)
-
+	//localhost:8888
+	log.Println("Server started at localhost:8888")
+	log.Fatal(http.ListenAndServe(":8888", nil))
 }
