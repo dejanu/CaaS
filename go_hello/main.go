1cf955851ee979df3a48c5afbacd4b4aa437b45b
dejanualex <dejanualexandru@gmail.com>
2025-01-20 23:45:24 +0200

    alling apps

--- go_hello/main.go
+++ go_hello/main.go
@@ -7,19 +7,17 @@ import (
 )
 
 func main() {
-	// / endpoint
+	// /
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// fmt.Fprintf(w, "<p><hr><center>GO <b>LION</b>🦁</center></p>")
+		fmt.Fprintf(w, "<p><hr><center>Hello <b>GOPHER</b> 👾 </center></p>")
+	})
+
+	// /hi
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./static/gopher_dance.gif") // serve static file
 		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
 	})
 
-	// /hi endpoint
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hellooo woooorld")
-	})
-
-	//localhost:8888
 	log.Println("Server started at localhost:8888")
 	log.Fatal(http.ListenAndServe(":8888", nil))
 }
